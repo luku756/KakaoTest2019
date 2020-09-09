5c9d3d80a426f08572324ba09285bf4b8cb2ea5b
luku756 <luku756@gmail.com>
2020-09-10 00:04:00 +0900

    temp

--- KakaoTest2019/p7.cpp
+++ KakaoTest2019/p7.cpp
@@ -348,7 +348,7 @@ int remove(vector<vector<int>>& board, int num) {
 	return -1;
 }
 
-int solution(vector<vector<int>> board) {
+int old_sol(vector<vector<int>> board) {
 	int answer = 0;
 
 	for (int i = 0; i < 202; i++)
@@ -422,6 +422,99 @@ int solution(vector<vector<int>> board) {
 	//	printf("\n");
 	//}
 
+	return answer;
+}
+
+void fillLine(vector<vector<int>>& board, int x) {
+
+	int n = board.size();
+	for (int i = 0; i < n; i++) {
+
+		if (board[i][x] > 0)
+			break;
+
+		board[i][x] = -2;
+
+	}
+
+
+}
+
+int checkrect(vector<vector<int>>& board, int x, int y) {
+	int n = board.size();
+	int num = board[y][x];
+
+	int numcnt = 0;
+	int boxcnt = 0;
+
+	int id_x[4][2] = { {0,2}, {0,2} , {0,1} , {0, 1} };
+	int id_y[4][2] = { {-1,0}, {0,1} , {0,2} , {-2,0} };
+
+	for (int t = 0; t < 4; t++) {
+		numcnt = 0;
+		boxcnt = 0;
+		//ooo
+		//xoo
+		for (int i = id_x[t][0]; i <= id_x[t][1]; i++) {
+			for (int j = id_y[t][0]; j <= id_y[t][1]; j++) {
+				int newx = i + x, newy = j + y;
+				if (0 <= newx && newx < n && 0 <= newy && newy < n) {
+					if (board[newy][newx] == num)
+						numcnt++;
+					if (board[newy][newx] == -2)
+						boxcnt++;
+				}
+			}
+		}
+
+		if (boxcnt == 2 && numcnt == 4) {
+			//printf("find remove target : %d, num %d\n",t,num);
+			//print(board);
+			for (int i = id_x[t][0]; i <= id_x[t][1]; i++) {
+				for (int j = id_y[t][0]; j <= id_y[t][1]; j++) {
+					int newx = i + x, newy = j + y;
+					board[newy][newx] = 0;					
+				}
+			}
+
+			for (int i = id_x[t][0]; i <= id_x[t][1]; i++) {
+				fillLine(board,i + x);
+			}
+
+			//print(board);
+			return 1;
+		}
+	}
+
+	return 0;
+
+}
+
+int solution(vector<vector<int>> board) {
+
+	//print(board);
+	int n = board.size();
+	for (int i = 0; i < n; i++) {
+		fillLine(board, i);
+	}
+
+
+	int answer = 0;
+	int res;
+	int ck = -1;
+
+	while (answer != ck) {
+		ck = answer;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[j][i] > 0) {
+					res = checkrect(board, i, j);
+					answer += res;
+				}
+			}
+		}
+	}
+	
 	return answer;
 }
 
